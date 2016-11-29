e3c34d33de42f07520da1e295eb234798294cab2
Greg Swallow <gswallow@gmail.com>
2016-11-29 17:14:05 -0500

    To date, we haven't created a single io1 volume because AWS is so awesome.

--- lib/sparkleformation/registry/ebs_volumes.rb
+++ lib/sparkleformation/registry/ebs_volumes.rb
@@ -1,10 +1,8 @@
 SfnRegistry.register(:ebs_volumes) do |options = {}|
 
-  volume_type = options.fetch(:volume_type, 'gp2')
   volume_count = options.fetch(:volume_count, 0).to_i
   volume_size = options.fetch(:volume_size, 0)
   delete_on_termination = options.fetch(:delete_on_termination, 'true')
-  iops = options.fetch(:iops, 0)
   root_vol_size = options.fetch(:root_vol_size, '12')
 
   array!(
@@ -13,20 +11,17 @@ SfnRegistry.register(:ebs_volumes) do |options = {}|
       ebs do
         delete_on_termination 'true'
         volume_type 'gp2'
-        volume_size root_vol_size.to_s
+        volume_size root_vol_size
       end
     },
-      *(0...volume_count).map { |c| -> {
-        device_name "/dev/sd#{(102 + c).chr}" # f, g, h, i...
-        ebs do
-          if volume_type == 'io1'
-            iops (iops == 0 ? 3 * volume_size : iops).to_s
-          end
-          delete_on_termination delete_on_termination
-          volume_type volume_type
-          volume_size volume_size.to_s
-        end
-        }
+    *(0...volume_count).map { |c| -> {
+      device_name "/dev/sd#{(102 + c).chr}" # f, g, h, i...
+      ebs do
+        delete_on_termination delete_on_termination
+        volume_type 'gp2'
+        volume_size volume_size
+      end
       }
+    }
   )
 end
