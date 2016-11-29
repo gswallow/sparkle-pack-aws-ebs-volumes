22ae16654db972afa8c71660e56beb5a86b055f4
Greg Swallow <gswallow@gmail.com>
2016-11-29 15:10:42 -0500

    Use splat operator to spit out the elements of an array, not an array itself.

--- lib/sparkleformation/registry/ebs_volumes.rb
+++ lib/sparkleformation/registry/ebs_volumes.rb
@@ -1,11 +1,11 @@
 SfnRegistry.register(:ebs_volumes) do |options = {}|
 
-  volume_type = options.fetch('volume_type', 'gp2')
-  volume_count = options.fetch('volume_count', 0).to_i - 1
-  volume_size = options.fetch('volume_size', 0)
-  delete_on_termination = options.fetch('delete_on_termination', 'true')
-  iops = options.fetch('iops', 0)
-  root_vol_size = options.fetch('root_vol_size', '12')
+  volume_type = options.fetch(:volume_type, 'gp2')
+  volume_count = options.fetch(:volume_count, 0).to_i
+  volume_size = options.fetch(:volume_size, 0)
+  delete_on_termination = options.fetch(:delete_on_termination, 'true')
+  iops = options.fetch(:iops, 0)
+  root_vol_size = options.fetch(:root_vol_size, '12')
 
   array!(
     -> {
@@ -16,8 +16,7 @@ SfnRegistry.register(:ebs_volumes) do |options = {}|
         volume_size root_vol_size.to_s
       end
     },
-    if volume_count > 0
-      (0...volume_count).map { |c| -> {
+      *(0...volume_count).map { |c| -> {
         device_name "/dev/sd#{(102 + c).chr}" # f, g, h, i...
         ebs do
           if volume_type == 'io1'
@@ -29,6 +28,5 @@ SfnRegistry.register(:ebs_volumes) do |options = {}|
         end
         }
       }
-    end
   )
 end
