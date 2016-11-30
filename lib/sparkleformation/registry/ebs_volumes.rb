0da6dda692cb7075b5c9eec59aff198439fd338d
Greg Swallow <gswallow@gmail.com>
2016-11-30 18:56:37 -0500

    Add conditionals for io1 volumes.

--- lib/sparkleformation/registry/ebs_volumes.rb
+++ lib/sparkleformation/registry/ebs_volumes.rb
@@ -2,6 +2,7 @@ SfnRegistry.register(:ebs_volumes) do |options = {}|
 
   volume_count = options.fetch(:volume_count, 0).to_i
   volume_size = options.fetch(:volume_size, 0)
+  provisioned_iops = options.fetch(:provisioned_iops, 300)
   delete_on_termination = options.fetch(:delete_on_termination, 'true')
   root_vol_size = options.fetch(:root_vol_size, '12')
 
@@ -18,7 +19,8 @@ SfnRegistry.register(:ebs_volumes) do |options = {}|
       device_name "/dev/sd#{(102 + c).chr}" # f, g, h, i...
       ebs do
         delete_on_termination delete_on_termination
-        volume_type 'gp2'
+        volume_type if!(options[:io1_condition], 'io1', 'gp2')
+        iops if!(options[:io1_condition], provisioned_iops, no_value!)
         volume_size volume_size
       end
       }
